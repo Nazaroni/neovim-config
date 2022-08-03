6cf670a92f0a0f3e1cffab279565734a1dea4745
Nazaroni <nazar.malyy@gmail.com>
2022-08-03 23:02:18 +0200

    unified icons for all OS's

--- lua/user/icons.lua
+++ lua/user/icons.lua
@@ -2,9 +2,12 @@
 -- go to the above and then enter <c-v>u<unicode> and the symbold should appear
 -- or go here and upload the font file: https://mathew-kurian.github.io/CharacterMap/
 -- find more here: https://www.nerdfonts.com/cheat-sheet
-vim.g.use_nerd_icons = false
-if vim.fn.has "mac" == 1 or vim.g.use_nerd_icons then
-  -- elseif vim.fn.has "mac" == 1 then
+
+
+-- print(vim.inspect(vim.fn.has "linux"))
+
+
+if vim.fn.has "mac" == 1 or vim.fn.has "windows" == 1 then
   return {
     kind = {
       Text = "",
